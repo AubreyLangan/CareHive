9e8a789f42c85ad82a23dac252dcdbf3bbd8f80a
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-11 12:50:57 -0700

    Initial Commit 11 June 2024

---
+++ Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+    return (
+        <footer className="bg-light text-center text-lg-start">
+            <Container className="p-4">
+                <Row>
+                    <Col lg="6" className="mb-4 mb-lg-0">
+                        <h5 className="text-uppercase">CareHive</h5>
+                        <p>
+                            A great place for parents to find tips, activities, and connect with the community.
+                        </p>
+                    </Col>
+                    <Col lg="6" className="text-center text-lg-end">
+                        <ul className="list-unstyled mb-0">
+                            <li><a href="/" className="text-dark">Home</a></li>
+                            <li><a href="/tips" className="text-dark">Tips</a></li>
+                            <li><a href="/activities" className="text-dark">Activities</a></li>
+                            <li><a href="/community" className="text-dark">Community</a></li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
