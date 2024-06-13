5a310965abe892978487a9360de0fada408a1a09
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-13 13:46:03 -0700

    Commit 2 13 June 2024

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to CareHive</h1>
+            <p>
+                Your one-stop solution for parenting tips, activities, and community support.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
