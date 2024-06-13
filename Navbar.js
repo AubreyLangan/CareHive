a28e3c35f633a55b2e86ef887a2a87feecd8ee70
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-13 13:41:54 -0700

    Commit 13 June 2024

--- Navbar.js
+++ Navbar.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import './Navbar.css';
 
 const CustomNavbar = () => {
     return (
-        <Container>
-            <Navbar.Brand href='/'>Parenting App</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='me-auto'>
-                    <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/tips">
-                        <Nav.Link>Tips</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/activities">
-                        <Nav.Link>Activities</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/community">
-                        <Nav.Link>Community</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
+        <Navbar bg='light' expand='lg'>
+            <Container>
+                <Navbar.Brand href='/'></Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/tips">
+                            <Nav.Link>Tips</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/activities">
+                            <Nav.Link>Activities</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/community">
+                            <Nav.Link>Community</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
