0523afeb04320427dc2435a2422138c278b457ae
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-13 13:55:29 -0700

    Commit 2 13 June 2024

---
+++ Activities.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Activities.css';
+
+const Activities = () => {
+    return (
+        <div>
+            <h1>Fun Activities</h1>
+            <p>
+                Discover fun and educational activities for your children.
+            </p>
+        </div>
+    );
+};
+
+export default Activities;
