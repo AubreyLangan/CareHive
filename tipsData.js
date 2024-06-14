b095539dacc312676245085cc18d8d0970d440f4
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-14 09:53:19 -0700

    Initial Commit 14 June 2024

---
+++ tipsData.js
@@ -0,0 +1,9 @@
+const tipsData = [
+    "Spend quality time with your children.",
+    "Listen to your child's needs and concerns.",
+    "Establish a routine and stick to it.",
+    "Encourage open communication.",
+    "Model good behavior and values.",
+];
+
+export default tipsData;
