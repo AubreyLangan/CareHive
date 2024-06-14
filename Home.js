121183039e5170997036282e82b77e5771b3186b
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-14 10:36:46 -0700

    Commit 14 June 2024

--- Home.js
+++ Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import './Home.css';
+import RandomTipGenerator from "../Components/RandomTipGenerator";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to CareHive</h1>
-            <p>
-                Your one-stop solution for parenting tips, activities, and community support.
-            </p>
+        <div className="home">
+            <header className="home-header">
+                <h1>Welcome to CareHive</h1>
+                <p>
+                    Your one-stop solution for parenting tips, activities, and community support.
+                </p>
+            </header>
+
+            <section className="random-tip-section">
+                <RandomTipGenerator />
+            </section>
         </div>
     );
 };
