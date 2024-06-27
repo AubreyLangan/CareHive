36c58f7c63b8ee8b3f02ead01d75fd2661eb0574
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-27 11:08:36 -0700

    Commit 27 June 2024
    Return button added

--- Toys6To8.js
+++ Toys6To8.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 const toyData = [
     {title: "Puzzle Games", description: "Interactive puzzle games."},
@@ -22,6 +23,11 @@ const Toys6To8 = () => {
                     </Col>
                 ))}
             </Row>
+            <div className="text-center mt-4">
+                <Link to="/toys">
+                    <Button variant="primary">Back to Toys</Button>
+                </Link>
+            </div>
         </Container>
     );
 };
