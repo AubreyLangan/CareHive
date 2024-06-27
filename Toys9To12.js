7a507dba085624807fa79f7f386037cf4992cd28
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-27 11:11:28 -0700

    Commit 27 June 2024
    Return button added

--- Toys9To12.js
+++ Toys9To12.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const toyData = [
     {title: "Board Games", description: "Fun and engaging board games."},
@@ -22,6 +23,11 @@ const Toys9To12 = () => {
                     </Col>
                 ))}
             </Row>
+            <div className="text-center mt-4">
+                <Link to="/toys">
+                    <Button variant="primary">Back to Toys</Button>
+                </Link>
+            </div>
         </Container>
     );
 };
