4832e186596e7ebf5abccb19ff81e3bb31fafdac
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-28 09:39:40 -0700

    Commit 28 June 2024
    Fixes to make it more visually appealing and take up less space

--- Footer.js
+++ Footer.js
@@ -4,24 +4,15 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="bg-light text-center text-lg-start">
-            <Container className="p-4">
-                <Row>
-                    <Col lg="6" className="mb-4 mb-lg-0">
-                        <h5 className="text-uppercase">CareHive</h5>
-                        <p>
-                            A great place for parents to find tips, activities, and connect with the community.
-                        </p>
+        <footer className="bg-dark text-light py-3">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6} className="text-center text-md-left mb-2 mb-md-0">
+                        <p className="mb-0"> 2024 CareHive. All Rights Reserved.</p>
                     </Col>
-                    <Col lg="6" className="text-center text-lg-end">
-                        <ul className="list-unstyled mb-0">
-                            <li><a href="/" className="text-dark">Home</a></li>
-                            <li><a href="/about" className="text-dark">About</a></li>
-                            <li><a href="/tips" className="text-dark">Tips</a></li>
-                            <li><a href="/activities" className="text-dark">Activities</a></li>
-                            <li><a href="/community" className="text-dark">Community</a></li>
-                            <li><a href="/toys" className="text-dark">Toys</a></li>
-                        </ul>
+                    <Col md={6} className="text-center text-md-right">
+                        <a href="/privacy" className="text-light mx-2">Privacy Policy</a>
+                        <a href="/terms" className="text-light mx-2">Terms of Service</a>
                     </Col>
                 </Row>
             </Container>
