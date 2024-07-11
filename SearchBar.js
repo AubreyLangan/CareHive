880f456bd739146428867afeaee3c6ae91cf83be
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-07-11 09:49:44 -0700

    Commit 2 11 July 2024

--- SearchBar.js
+++ SearchBar.js
@@ -7,12 +7,19 @@ const SearchBar = ({ onSearch }) => {
         onSearch(query);
     };
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
         <div className="search-bar">
             <input
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={handleKeyDown}
                 placeholder="Search..."
             />
             <button onClick={handleSearch}>Search</button>
